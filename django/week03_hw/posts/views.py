7fa90b4e2754f40b48900cf1a4d26635104dd8e5
jwdebbie <leejoowon09@gmail.com>
2025-05-11 21:25:06 +0900

    Refactor : views.py 수정

--- django/week03_hw/posts/views.py
+++ django/week03_hw/posts/views.py
@@ -15,3 +15,44 @@ def post_form_view(request):
         return render(request, 'posts/post_form2.html', context)
     else:
         return redirect('index')
+
+def index(request):
+    return render(request, 'index.html')
+
+def post_list_view(request):
+    return render(request, 'posts/post_list.html')
+
+def post_detail_view(request, id):
+    return render(request, 'posts/post_detail.html')
+
+def post_create_view(request):
+    return render(request, 'posts/post_form.html')
+
+def post_update_view(request, id):
+    return render(request, 'posts/post_update.html')
+
+def post_delete_view(request, id):
+    return render(request, 'posts/post_confirm_delete.html')
+
+class class_view(ListView):
+    model = Post
+    template_name='cbv_view.html'
+
+
+def url_view(request):
+    data = {'code':'001','msg':'OK'}
+    return HttpResponse('<h1>url_views</h1>')
+
+def url_parameter_view(request, username):
+    print('url_parameter_view()')
+    print(f'username: {username}')
+    print(f'request.GET: {request.GET}')
+    return HttpResponse(username)
+
+def function_view(request):
+    print(f'request.method:{request.method}')
+    if request.method == "GET":
+        print(f'request.GET: {request.GET}')
+    elif request.method == "POST":
+        print(f'request.POST: {request.POST}')
+    return render(request, 'view.html')
