9f22bd75e80bcbba7b02a0262c3a78e961402eae
jwdebbie <leejoowon09@gmail.com>
2025-06-29 17:28:55 +0900

    Feat : 댓글기능 urls 연결

--- django/5th_hw/blog/urls.py
+++ django/5th_hw/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PostListCreateAPIView, PostRetrieveUpdateDestroyAPIView
+from .views import PostListCreateAPIView, PostRetrieveUpdateDestroyAPIView, CommentListCreateAPIView, CommentRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('posts/', PostListCreateAPIView.as_view()),
     path('posts/<int:pk>/', PostRetrieveUpdateDestroyAPIView.as_view()),
+    path('comments/', CommentListCreateAPIView.as_view()),
+    path('comments/<int:pk>/', CommentRetrieveUpdateDestroyAPIView.as_view()),
 ]
