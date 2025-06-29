45362010aeae7cccaf9aad5b12974c43d086f3b5
jwdebbie <leejoowon09@gmail.com>
2025-06-29 17:14:44 +0900

    Feat : blog 앱에 urls.py 추가 및 프로젝트 urls.py 연결

--- django/5th_hw/blog/views.py
+++ django/5th_hw/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+# 게시글 전체 조회 + 생성
+class PostListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+# 게시글 단일 조회 + 수정 + 삭제
+class PostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
