858549b392d932d0f12158382d7d65390b0fe250
jwdebbie <leejoowon09@gmail.com>
2025-06-29 17:27:06 +0900

    Feat : Comment 기능 뷰 로직 구현

--- django/5th_hw/blog/views.py
+++ django/5th_hw/blog/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
 # 게시글 전체 조회 + 생성
 class PostListCreateAPIView(generics.ListCreateAPIView):
@@ -11,3 +11,13 @@ class PostListCreateAPIView(generics.ListCreateAPIView):
 class PostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+# 댓글 전체 조회 및 생성
+class CommentListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+# 댓글 단일 조회, 수정, 삭제
+class CommentRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
